0d20f632f4d769125008865099be935addc51cac
toluwaniolosunde-wq <toluwaniolosunde@gmail.com>
2026-02-16 10:07:55 +0100

    Add files via upload

---
+++ Object.js
@@ -0,0 +1,56 @@
+const person={
+
+firstName: "Mofi",
+lastName: "Ogunniyi",
+age: 11,
+}
+
+console.log(person)
+
+person.email="mofifoluwaogunniyi@gmail.com"
+
+console.log(person)
+
+person.age=19
+
+console.log(person)
+
+delete person.lastName;
+
+console.log(person)
+
+
+console.log(person)
+
+
+const student={
+
+name: "Mofi",
+age: 11,
+grade:49
+
+}
+
+console.log(student)
+
+student.school="semicolon"
+
+student.age=19
+
+console.log(student)
+
+delete student.age
+
+console.log(student)
+console.log()
+
+console.log(Object.keys(student))
+console.log(Object.values(student))
+
+
+
+
+
+
+
+
